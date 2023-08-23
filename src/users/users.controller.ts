8fd39dfa59f29be281d78ef321429ebbbdc5ac6a
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-08-23 20:49:16 +0900

    feat : 최초 로그인 설정 API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Res, Req, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Res,
+  Req,
+  Get,
+  UseGuards,
+  Post,
+  Put,
+  Body,
+  HttpStatus,
+} from '@nestjs/common';
 import { Response } from 'express';
-import { AuthService } from 'src/auth/auth.service';
 import { GoogleAuthGuard } from 'src/auth/guard/google-auth.guard';
 import { KakaoAuthGuard } from 'src/auth/guard/kakao-auth.guard';
 import { NaverAuthGuard } from 'src/auth/guard/naver-auth.guard';
+import { UsersService } from './users.service';
+import { AddUserInfoDto } from './dto/addUserInfo-user.dto';
 
 @Controller()
 export class UsersController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly userService: UsersService) {}
 
   /* 카카오 로그인 서비스*/
   @UseGuards(KakaoAuthGuard)
-  @Get('auth/kakao')
+  @Get('api/auth/kakao')
   async kakaoLogin() {
     return;
   }
@@ -26,7 +37,7 @@ export class UsersController {
 
   /*네이버 로그인 서비스*/
   @UseGuards(NaverAuthGuard)
-  @Get('auth/naver')
+  @Get('api/auth/naver')
   async naverLogin() {
     return;
   }
@@ -39,7 +50,7 @@ export class UsersController {
 
   /*구글 로그인 서비스*/
   @UseGuards(GoogleAuthGuard)
-  @Get('auth/google')
+  @Get('api/auth/google')
   async googleLogin() {
     return;
   }
@@ -48,5 +59,18 @@ export class UsersController {
   @Get('auth/google/callback')
   async googleCallback(@Req() req: any, @Res() res: Response) {
     res.redirect('http://localhost:3000');
+  }
+
+  /*최초 로그인 설정*/
+  @Put('api/auth/info')
+  async addUserInfo(
+    @Body() addUserInfoDto: AddUserInfoDto,
+    @Res() res: any,
+    @Req() req: any,
+  ): Promise<any> {
+    await this.userService.addUserInfo(addUserInfoDto);
+    return res
+      .status(HttpStatus.CREATED)
+      .json({ message: '추가 정보 입력 완료' });
   }
 }
