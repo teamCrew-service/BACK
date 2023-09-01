1dbe81ee4e2aa9d90abdb5a45990d136603a165d
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-09-01 13:10:14 +0900

    swagger 작성 및 일부 API 수정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); //글로벌 프리픽스 설정
+
+  const config = new DocumentBuilder()
+    .setTitle('CREW API Document')
+    .setDescription('Crew API 서버')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/signup/dto/submit-signup.dto.ts
+++ src/signup/dto/submit-signup.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger/dist';
 import { IsString } from 'class-validator';
 
 export class SubmitSignupDto {
   // answer1
+  @ApiProperty({
+    example: '운정 물주먹입니다.',
+    description: '답1',
+    required: true,
+  })
   @IsString()
   readonly answer1: string;
 
   //answer2
+  @ApiProperty({
+    example: '책임감 있는, 부지런한, 웃긴',
+    description: '답2',
+    required: true,
+  })
   @IsString()
   readonly answer2: string;
 }
