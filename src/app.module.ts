6f42ea751e93b28bd509d4700f74908067eda686
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-09-10 20:28:10 +0900

    crew 수정 삭제 미들웨어 적용 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,6 +51,8 @@ export class AppModule implements NestModule {
       { path: 'mypage', method: RequestMethod.GET },
       { path: 'auth/info', method: RequestMethod.PUT },
       { path: 'crew/createcrew', method: RequestMethod.POST },
+      { path: ':crewId/edit', method: RequestMethod.PUT },
+      { path: ':crewId/delete', method: RequestMethod.DELETE },
       {
         path: 'signup/:crewId/:signupFormId/submit',
         method: RequestMethod.POST,
