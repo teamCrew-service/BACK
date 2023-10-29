bdb6df9661f9fcf271c1de0aaeef6516b4541ae3
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-10-29 20:09:31 +0900

    leavecrew 에러, 주석 처리 및 가독성을 위한 정리

--- src/leavecrew/leavecrew.repository.ts
+++ src/leavecrew/leavecrew.repository.ts
@@ -12,45 +12,64 @@ export class LeavecrewRepository {
 
   /* 등록한 탈퇴 명단 삭제해주기 */
   async findAllLeaveCrew(): Promise<any> {
-    const koreaTimezoneOffset = 9 * 60;
-    const currentDate = new Date();
-    const today = new Date(currentDate.getTime() + koreaTimezoneOffset * 60000);
-    const toBeLeaveCrew = await this.leavecrewRepository
-      .createQueryBuilder('leavecrew')
-      .delete()
-      .from(Leavecrew)
-      .where('leavecrew.leaveDay = :today', { today })
-      .execute();
-    return toBeLeaveCrew;
+    try {
+      const koreaTimezoneOffset = 9 * 60;
+      const currentDate = new Date();
+      const today = new Date(
+        currentDate.getTime() + koreaTimezoneOffset * 60000,
+      );
+      const toBeLeaveCrew = await this.leavecrewRepository
+        .createQueryBuilder('leavecrew')
+        .delete()
+        .from(Leavecrew)
+        .where('leavecrew.leaveDay = :today', { today })
+        .execute();
+      return toBeLeaveCrew;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LeavecrewRepository/findAllLeaveCrew');
+    }
   }
 
   /* crew 탈퇴 등록하기 */
   async createLeaveCrew(crewId: number, userId: number): Promise<any> {
-    const koreaTimezoneOffset = 9 * 60;
-    const currentDate = new Date();
-    const today = new Date(currentDate.getTime() + koreaTimezoneOffset * 60000);
+    try {
+      const koreaTimezoneOffset = 9 * 60;
+      const currentDate = new Date();
+      const today = new Date(
+        currentDate.getTime() + koreaTimezoneOffset * 60000,
+      );
 
-    // 7일 뒤 계산
-    const sevenDaysLater = new Date(today);
-    sevenDaysLater.setDate(today.getDate() + 7);
+      // 7일 뒤 계산
+      const sevenDaysLater = new Date(today);
+      sevenDaysLater.setDate(today.getDate() + 7);
 
-    const leaveUser = new Leavecrew();
-    leaveUser.crewId = crewId;
-    leaveUser.userId = userId;
-    leaveUser.leaveDay = sevenDaysLater;
-    await this.leavecrewRepository.save(leaveUser);
-    return;
+      const leaveUser = new Leavecrew();
+      leaveUser.crewId = crewId;
+      leaveUser.userId = userId;
+      leaveUser.leaveDay = sevenDaysLater;
+      await this.leavecrewRepository.save(leaveUser);
+      return;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LeavecrewRepository/createLeaveCrew');
+    }
   }
 
   /* crew 탈퇴자 조회하기 */
   async findOneLeaveUser(crewId: number, userId: number): Promise<any> {
-    const leaveUser = await this.leavecrewRepository
-      .createQueryBuilder('leavecrew')
-      .select(['crewId', 'leaveDay'])
-      .where('leavecrew.userId = :userId', { userId })
-      .andWhere('leavecrew.crewId = :crewId', { crewId })
-      .getRawOne();
+    try {
+      const leaveUser = await this.leavecrewRepository
+        .createQueryBuilder('leavecrew')
+        .select(['crewId', 'leaveDay'])
+        .where('leavecrew.userId = :userId', { userId })
+        .andWhere('leavecrew.crewId = :crewId', { crewId })
+        .getRawOne();
 
-    return leaveUser;
+      return leaveUser;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LeavecrewRepository/findOneLeaveUser');
+    }
   }
 }
