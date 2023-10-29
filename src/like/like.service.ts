095d0092d153b9b667e2f0a5550358521711b126
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-10-29 20:16:16 +0900

    Like 에러, 주석 처리 및 가독성을 위한 정리

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -7,42 +7,72 @@ export class LikeService {
 
   /* 찜하기 */
   async likeCrew(crewId: number, userId: number): Promise<any> {
-    const like = await this.likeRepository.likeCrew(crewId, userId);
-    return like;
+    try {
+      const like = await this.likeRepository.likeCrew(crewId, userId);
+      return like;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/likeCrew');
+    }
   }
 
   /* 찜 취소하기 */
   async cancelLikeCrew(crewId: number, userId: number): Promise<any> {
-    const caceledLike = await this.likeRepository.cancelLikeCrew(
-      crewId,
-      userId,
-    );
-    return caceledLike;
+    try {
+      const caceledLike = await this.likeRepository.cancelLikeCrew(
+        crewId,
+        userId,
+      );
+      return caceledLike;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/cancelLikeCrew');
+    }
   }
 
   /* 찜 조회하기 */
   async findLikedCrew(userId: number): Promise<any> {
-    const likedCrewId = await this.likeRepository.findLikedCrew(userId);
-    return likedCrewId;
+    try {
+      const likedCrewId = await this.likeRepository.findLikedCrew(userId);
+      return likedCrewId;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/findLikedCrew');
+    }
   }
 
   /* crew를 찜한 횟수 확인 */
   async countLikedCrew(crewId: number): Promise<any> {
-    const likedCrew = await this.likeRepository.countLikedCrew(crewId);
+    try {
+      const likedCrew = await this.likeRepository.countLikedCrew(crewId);
 
-    const likeCount = likedCrew.length;
-    return likeCount;
+      const likeCount = likedCrew.length;
+      return likeCount;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/countLikedCrew');
+    }
   }
 
   /* user가 crew를 찜했는지 확인하기 */
   async confirmLiked(crewId: number, userId: number): Promise<any> {
-    const like = await this.likeRepository.confirmLiked(crewId, userId);
-    return like;
+    try {
+      const like = await this.likeRepository.confirmLiked(crewId, userId);
+      return like;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/confirmLiked');
+    }
   }
 
   /* 좋아요 삭제 */
   async deleteLike(crewId: number): Promise<any> {
-    const deleteLike = await this.likeRepository.deleteLike(crewId);
-    return deleteLike;
+    try {
+      const deleteLike = await this.likeRepository.deleteLike(crewId);
+      return deleteLike;
+    } catch (e) {
+      console.error(e);
+      throw new Error('LikeService/deleteLike');
+    }
   }
 }
