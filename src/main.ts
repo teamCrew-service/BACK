858237be235ec6c8c43675006a37ae5a54955570
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-10-29 23:02:17 +0900

    app 주석 처리

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors 설정
   app.enableCors({
     origin: ['http://localhost:3000', process.env.CORS_ORIGIN],
     // origin: 'http://localhost:80',
@@ -13,10 +14,11 @@ async function bootstrap() {
     credentials: true,
     optionsSuccessStatus: 204,
   });
-  app.use(cookieParser());
+  app.use(cookieParser()); // cookie 설정
   app.useGlobalPipes(new ValidationPipe()); // 입력값 유효성 검사를 위한 ValidationPipe 추가
   app.setGlobalPrefix('api'); //글로벌 프리픽스 설정
 
+  // swagger 설정
   const config = new DocumentBuilder()
     .setTitle('CREW API Document')
     .setDescription('Crew API 서버')
